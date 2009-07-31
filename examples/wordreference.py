a7cd0ae570367ff49ebadd8a2f5cab8e90276109
tokland <tokland@cd079206-7943-11de-bdc7-1f637afd43b1>
2009-07-31 16:22:16 +0000

    add a URL filter
    git-svn-id: http://spynner.googlecode.com/svn/trunk@97 cd079206-7943-11de-bdc7-1f637afd43b1

--- examples/wordreference.py
+++ examples/wordreference.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 import spynner
 import pyquery
+import os
 
-browser = spynner.Browser(debug_level=spynner.INFO, html_parser=pyquery.PyQuery)
+def images_filter(operation, url):        
+    return os.path.splitext(url)[1] not in (".jpg", ".png", ".gif")
+  
+browser = spynner.Browser(debug_level=spynner.INFO)
+browser.set_html_parser(pyquery.PyQuery)
+browser.set_url_filter(images_filter)
 browser.create_webview()
 browser.show()
 browser.load("http://www.wordreference.com")
@@ -14,4 +20,4 @@ browser.soup.make_links_absolute(base_url=browser.url)
 print "html:", browser.soup("#Otbl").html()
 data = browser.download(browser.soup("img:first").attr('src'))
 print "image length:", len(data)
-browser.browse()
+browser.close()
