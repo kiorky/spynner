fc46aca5a8c1a7ad0c3db47744c3cd9de78a4d37
tokland <tokland@cd079206-7943-11de-bdc7-1f637afd43b1>
2010-07-14 17:53:10 +0000

    example with google pag
    git-svn-id: http://spynner.googlecode.com/svn/trunk@140 cd079206-7943-11de-bdc7-1f637afd43b1

---
+++ examples/google.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import spynner
+import pyquery
+
+browser = spynner.Browser(debug_level=spynner.INFO)
+browser.create_webview()
+browser.show()
+browser.load("http://www.wordreference.com")
+browser.choose("input[name=lr=lang_es]")
+browser.click("input[name=enit]")
+browser.click("a[class=l]:first")
+d = pyquery.PyQuery(browser.html)
+d.make_links_absolute(base_url=browser.url)
+href = d('a:last').attr('href')
+print href
+print len(browser.download(href))
+browser.browse()
