408e88243005166a47b9d44b02fcb8268c70a484
kiorky <kiorky@cryptelium.net>
2011-05-29 22:07:42 +0200

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 import os
 from distutils.cmd import Command
 
-version = '1.1'
+version = '1.2dev'
 url = "https://github.com/kiorky/spynner"
 
 class gen_doc(Command):
