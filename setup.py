91d2e77c378c997113ffefd6c0e977606db3573d
kiorky <kiorky@cryptelium.net>
2011-05-29 22:41:02 +0200

    Preparing release 1.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 import os
 from distutils.cmd import Command
 
-version = '1.7'
+version = '1.8'
 url = "https://github.com/kiorky/spynner"
 
 class gen_doc(Command):
