093da05e72f6d228794f3da975229bdb97e065c8
kiorky <kiorky@cryptelium.net>
2011-05-29 22:30:22 +0200

    release 1.4

--- setup.py
+++ setup.py
@@ -70,10 +70,10 @@ setup(
     ),
     data_files = [
         ('share/doc/spynner/examples',
-            'examples/wordreference.py',
+            ('examples/wordreference.py',
             'examples/webkit_methods.py',
             'examples/native_events.py',
-            'examples/google.py',),
+            'examples/google.py',)),
         ('share/spynner/javascript',
             ('src/spynner/javascript/jquery.min.js',
              'src/spynner/javascript/jquery.simulate.js')),
