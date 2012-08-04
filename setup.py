5da0b7fcdd3ed7a2e18549fc9324587b41edbfc8
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-08-04 14:51:39 +0200

    Preparing release 1.11

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 import os
 from distutils.cmd import Command
 
-version = '1.11dev'
+version = '1.11'
 url = "https://github.com/makinacorpus/spynner"
 
 class gen_doc(Command):
