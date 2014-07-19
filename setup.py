c205db628ac41c46f3df07345f547f0fa8ee25ff
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-07-19 17:39:03 +0200

    Back to development: 2.20

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 import os
 from distutils.cmd import Command
 
-version = '2.19'
+version = '2.20.dev0'
 url = "https://github.com/makinacorpus/spynner"
 
 def read(rnames):
